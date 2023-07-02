9cbd976662aa2acec9aedfc4ec1e18cb14584ab6
Mario Toruño <80336294+MarioLanza25@users.noreply.github.com>
2023-07-02 12:53:01 -0600

    Javascript para ventana pop up

--- nodejs/p.js
+++ nodejs/p.js
@@ -1,14 +1,23 @@
-const btnCrearCuenta=document.querySelector('.btn-crear-cuenta');
+//const btnCrearCuenta=document.querySelector('.btn-crear-cuenta');
 
-const formFormulario=document.querySelector('.formulario-login');
+//const formFormulario=document.querySelector('.formulario-login');
 
-const btnSalirModal=document.querySelector('.modal-close');
+//const btnSalirModal=document.querySelector('.modal-close');
 
-btnCrearCuenta.addEventListener('click',(e)=>{
-  e.preventDefault();
-  formFormulario.classList.add('formulario-login--show');
+//btnCrearCuenta.addEventListener('click',(e)=>{
+  //e.preventDefault();
+  //formFormulario.classList.add('formulario-login--show');
+//});
+//btnSalirModal.addEventListener('click',(e)=>{
+  //e.preventDefault();
+  //formFormulario.classList.remove('formulario-login--show');
+//});
+
+document.querySelector("#btn-iniciar-sesion").addEventListener("click", function(){
+  document.querySelector(".popup").classList.add("active");
 });
-btnSalirModal.addEventListener('click',(e)=>{
-  e.preventDefault();
-  formFormulario.classList.remove('formulario-login--show');
+
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+  document.querySelector(".popup").classList.remove("active");
 });
+
